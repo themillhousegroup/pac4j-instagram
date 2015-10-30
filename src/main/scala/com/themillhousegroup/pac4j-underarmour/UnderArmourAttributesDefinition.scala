931d68d5a84b96a863efe52eb040948b3cca31f3
John Marshall <john@themillhousegroup.com>
2015-10-30 17:16:09 +1100

    Handle nested JSON in the Pac4J-idiomatic way

--- src/main/scala/com/themillhousegroup/pac4j-underarmour/UnderArmourAttributesDefinition.scala
+++ src/main/scala/com/themillhousegroup/pac4j-underarmour/UnderArmourAttributesDefinition.scala
@@ -2,6 +2,8 @@ package com.themillhousegroup.pac4junderarmour
 
 import org.pac4j.core.profile.converter.Converters
 import org.pac4j.oauth.profile.OAuthAttributesDefinition
+import org.pac4j.oauth.profile.JsonHelper
+import org.pac4j.oauth.profile.JsonObject
 
 object UnderArmourAttributesDefinition extends OAuthAttributesDefinition {
 
@@ -10,6 +12,8 @@ object UnderArmourAttributesDefinition extends OAuthAttributesDefinition {
   val FIRST_NAME = "first_name"
   val LAST_NAME = "last_name"
   val DISPLAY_NAME = "display_name"
+
+  val LOCATION = "location"
   val LOCALITY = "locality"
   val REGION = "region"
   val COUNTRY = "country"
@@ -17,4 +21,24 @@ object UnderArmourAttributesDefinition extends OAuthAttributesDefinition {
 
   val EMAIL = "email"
 
+  addAttribute(FIRST_NAME, Converters.stringConverter)
+  addAttribute(LAST_NAME, Converters.stringConverter)
+  addAttribute(DISPLAY_NAME, Converters.stringConverter)
+
+  addAttribute(LOCATION, UnderArmourConverters.locationConverter)
+  addAttribute(GENDER, Converters.stringConverter)
+}
+
+class UnderArmourLocation(var country: String, var region: String, var locality: String) extends JsonObject {
+  import com.fasterxml.jackson.databind.JsonNode
+  override def buildFromJson(json: JsonNode): Unit = {
+    this.country = JsonHelper.convert(Converters.stringConverter, json, "country").asInstanceOf[String]
+    this.region = JsonHelper.convert(Converters.stringConverter, json, "region").asInstanceOf[String]
+    this.locality = JsonHelper.convert(Converters.stringConverter, json, "locality").asInstanceOf[String]
+  }
+}
+
+object UnderArmourConverters {
+  import org.pac4j.oauth.profile.converter.JsonObjectConverter
+  val locationConverter: JsonObjectConverter = new JsonObjectConverter(classOf[UnderArmourLocation])
 }
